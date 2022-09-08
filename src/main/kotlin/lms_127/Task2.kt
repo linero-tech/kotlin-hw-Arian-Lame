44fa4f28982af5f6e6ccd15e4ae51b9b5998ea8e
Arian-Lame <arianlame@hotmail.com>
2022-09-08 19:02:12 +0200

    task 2

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -5,7 +5,7 @@ fun task2() {
     val Name : String = "Arian"
     val Country: String = "Albania"
     val Month: String = "April"
-println( "Hello $Name, you are from $Country and you  born in $Month")
+println( "Hello $Name, you are from $Country and you were born in $Month")
 
 }
 fun main(){
