2dff70088e9cd3cf74f36655d14d9027c9ffba80
Arian-Lame <arianlame@hotmail.com>
2022-09-12 21:01:03 +0200

    solution to task 1,2,3,4

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -2,15 +2,13 @@ package lms_127
 
 
 fun task1() {
-
-    val country: String = "Albania"
-    val capital: String = "Tirane"
-    val currency: String = "LEK"
+    val country ="Albania"
+    val capital ="Tirane"
+    val currency ="LEK"
     println ("Country: $country")
     println ("Capital: $capital")
     println ("Currency: $currency")
 }
-
 fun main() {
     task1()
 }
--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -2,9 +2,9 @@ package lms_127
 
 fun task2() {
 
-    val name :String = "Arian"
-    val country:String = "Albania"
-    val month:String = "April"
+    val name = "Arian"
+    val country = "Albania"
+    val month = "April"
 println( "Hello $name, you are from $country and you were born in $month")
 
 }
