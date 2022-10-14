9f7032bcec2652153367c92cac2914d4208b6688
Arian-Lame <arianlame@hotmail.com>
2022-10-14 21:52:23 +0200

    solution task 1..10 module 103

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -1,14 +1,15 @@
 package lms_129
 
 fun task1(a: Int, b: Int): Int {
-    var result = 15
+    var result = 0
 
-    if (a < b) {for (number in a..b) {
-        result += number
-    }
+    if (a < b) {
+        for (number in a..b) {
+            result = number + result
+        }
     }
     return result
 }
 fun main () {
-    println( task1( a = 3, b = 3))
+    println( task1( a = 1, b = 5 ))
 }
