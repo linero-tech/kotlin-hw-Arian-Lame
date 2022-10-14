df68cd0dda89acdd4283c00cdeb1574c11c48f40
Arian-Lame <arianlame@hotmail.com>
2022-10-14 21:25:27 +0200

    module 103 - task 1..10 solution

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -1,6 +1,18 @@
 package lms_129
 
 fun task1(a: Int, b: Int): Int {
+    var result = 15
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    if (a < b) {for (number in a..b) {
+        result = number + result
+    }
+    }
+
+    return result
+
+}
+fun main () {
+    println( task1( a = 3, b = 3))
+
+
 }
--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -1,6 +1,24 @@
 package lms_129
 
 fun task9(temperature: String): String {
+// separate number from unit
+    val magnitude = temperature.substring(0, temperature.length-1).toDouble()
+// check if number is C or F
+    val isCelsius = temperature.contains("C", ignoreCase = true)
+// IF C
+    val result = if (isCelsius){
+        // use formula to convert to F
+        val conversion = (9.0 * magnitude / 5.0) + 32.0
+        "${conversion.toInt()}F"
+    } else {
+        // use formula to convert to C
+        val conversion = (5.0 * (magnitude - 32.0)) / 9.0
+        "${conversion.toInt()}C"
+    }
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return result
+}
+
+fun main(){
+    println(task9("50F"))
 }
