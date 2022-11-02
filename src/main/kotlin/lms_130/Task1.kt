7ed48cca8eb0deeaedfc4a994aecad7a1da8d5b0
Arian-Lame <arianlame@hotmail.com>
2022-11-02 18:10:50 +0100

    solution module 104 update

--- src/main/kotlin/lms_130/Task1.kt
+++ src/main/kotlin/lms_130/Task1.kt
@@ -2,11 +2,11 @@ package lms_130
 
 fun task1(items: List<Int>): Int {
 
+val result = if (items.isNotEmpty()) items.random() else 0
 
-    return if (items.isNotEmpty()) items.random() else 0
-
+    return result
 
 }
 fun main () {
-    println(task1(listOf()))
+    println(task1(listOf(2,3,5,8)))
 }
