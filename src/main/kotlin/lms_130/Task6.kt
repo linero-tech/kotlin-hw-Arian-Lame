7575785edd3d80c8012d22e46af34a267350fb25
Arian-Lame <arianlame@hotmail.com>
2022-11-05 14:22:31 +0100

    solution task 1...13 module 104 final

--- src/main/kotlin/lms_130/Task6.kt
+++ src/main/kotlin/lms_130/Task6.kt
@@ -1,25 +1,15 @@
-package lms_130
 fun task6(sentence: String): String {
     var result = ""
-
-    for((index, letter) in sentence.withIndex()) {
-
-        result += if (index % 2==0) {
-            letter.uppercaseChar()
-        } else {
-            letter
+    for ( index in sentence.indices){
+        var letter = sentence[index]
+        if (index % 2 == 0 ){
+            letter = letter.toUpperCase()
         }
+        result += letter
     }
 
-    sentence.forEachIndexed { index,letter ->
-        result += if (index % 2==0) {
-            letter.uppercaseChar()
-        } else {
-            letter
-        }
-    }
     return result
 }
-fun main() {
-    println(task6(sentence = " I like Gothenburg "))
+fun main (){
+    println(task6("I like Stockholm"))
 }
