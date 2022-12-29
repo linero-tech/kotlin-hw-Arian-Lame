3c56101b005ad13da20dbcb471d911d7197cdbc2
Humberto Linero <17732781+hlinero@users.noreply.github.com>
2022-12-29 15:32:14 +0100

    fix: compilation error

--- src/main/kotlin/lms_131/Task5.kt
+++ src/main/kotlin/lms_131/Task5.kt
@@ -6,10 +6,11 @@ fun task5(mapa: Map<Int, Int>, constant: Int): List<Int> {
 
     return result
 }
+
 fun main() {
     println(task5(
-        mapa = mutableMapOf(1 to 100, 3 to 200, 4 to 500)
-
+        mapa = mutableMapOf(1 to 100, 3 to 200, 4 to 500),
+        constant = 10,
     ))
 }
 
